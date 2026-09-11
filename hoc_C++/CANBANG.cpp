9c5149c27103126c12667b831faec4b9212c55c1
93hai <syhai18022012@gmail.com>
2026-09-11 13:37:23 +0700

    sửa lỗi bài 1
    sửa map he 👍

--- hoc_C++/CANBANG.cpp
+++ hoc_C++/CANBANG.cpp
@@ -3,7 +3,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-map<bool> he[1000000005];
+map<int, bool> he;
 long long a[100005];
 
 int main(){
@@ -21,7 +21,7 @@ int main(){
 
     long long dem = 0;
     for(int i=0;i<n;i++){
-        if(he[a[i-k]] && he[a[i-k]]){
+        if(he[a[i]+k] && he[a[i]-k]){
             dem++;
         }
     }
