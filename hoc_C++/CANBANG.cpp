acceabd9a5ea099e05bb295806444f1cc55662bb
93hai <syhai18022012@gmail.com>
2026-09-11 13:00:13 +0700

    bài đầu tiên
    link bài
    
    https://lqdoj.edu.vn/problem/26hsg9hno2?

---
+++ hoc_C++/CANBANG.cpp
@@ -0,0 +1,31 @@
+//link bai giai https://lqdoj.edu.vn/problem/26hsg9hno2?
+
+#include<bits/stdc++.h>
+using namespace std;
+
+map<bool> he[1000000005];
+long long a[100005];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n,k;
+    cin >> n >> k;
+
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+
+        he[a[i]] = true;
+    }
+
+    long long dem = 0;
+    for(int i=0;i<n;i++){
+        if(he[a[i-k]] && he[a[i-k]]){
+            dem++;
+        }
+    }
+
+    cout << dem;
+    return 0;
+}
