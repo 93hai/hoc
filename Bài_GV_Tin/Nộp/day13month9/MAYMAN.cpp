95fdb81307d3ac04e6299c29dc66fb27eb8c6c8d
93hai <syhai18022012@gmail.com>
2026-09-13 12:01:06 +0700

    pass chx vậy
    4LL là cái j v

--- Bài_GV_Tin/Nộp/day13month9/MAYMAN.cpp
+++ Bài_GV_Tin/Nộp/day13month9/MAYMAN.cpp
@@ -5,8 +5,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    freopen("MAYMAN.INP", "r", stdin);
-    freopen("MAYMAN.OUT", "w", stdout);
+    //freopen("MAYMAN.INP", "r", stdin);
+    //freopen("MAYMAN.OUT", "w", stdout);
 
     long long n;
     cin >> n;
@@ -14,7 +14,7 @@ int main(){
     long long g = floor(sqrtl(n));
 
     long long dem = floor(g / 2);
-    long long tong = (g + 1) * (g / 2);
+    long long tong = 4LL * dem * (dem + 1) * (dem * 2 + 1) / 6;
 
     cout << dem << "\n" << tong;
     return 0;
