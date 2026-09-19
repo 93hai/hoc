79166580a02440e55f35dc5470662a1b8898f729
93hai <syhai18022012@gmail.com>
2026-09-19 20:48:30 +0700

    Thế mà vẫn sai bruh

--- hoc_C++/TONGNHOHONK.cpp
+++ hoc_C++/TONGNHOHONK.cpp
@@ -13,11 +13,25 @@ int main(){
         cin >> n >> k;
 
         vector<long long> a(n + 5);
-        map<long long, long long> he;
 
-        for(long long i=0;i<n;i++){
-            cin >> a[i];
-            he[a[i]]++;
+        for(long long j=0;j<n;j++){
+            cin >> a[j];
         }
+
+        sort(a.begin(), a.end());
+
+        long long dau = 0, cuoi = n-1;
+        long long dem = 0;
+        while(dau < cuoi){
+            if(a[dau] + a[cuoi] <= k){
+                dau++;
+                dem += cuoi - dau;
+                
+            }else{
+                cuoi--;
+            }
+        }
+        cout << dem << "\n";
     }
+    return 0;
 }
