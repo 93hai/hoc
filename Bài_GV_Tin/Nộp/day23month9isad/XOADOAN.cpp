1605ab83208f660e12003963d2ab2146a5be3162
93hai <syhai18022012@gmail.com>
2026-09-23 21:08:47 +0700

    đâs

--- Bài_GV_Tin/Nộp/day23month9isad/XOADOAN.cpp
+++ Bài_GV_Tin/Nộp/day23month9isad/XOADOAN.cpp
@@ -1,1 +1,46 @@
-#XO
+#include<bits/stdc++.h>
+using namespace std;
+
+const long long INF = 2e18;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("XOADOAN.INP", "r", stdin);
+    freopen("XOADOAN.OUT", "w", stdout);
+
+    long long n;
+    cin >> n;
+
+    vector<long long> a(n);
+    long long tong = 0;
+
+    for(long long i=1;i<=n;i++){
+        cin >> a[i];
+        tong += a[i];
+    }
+
+    long long s;
+    cin >> s;
+
+    long long can_tru = tong - s;
+
+    long long l=0;
+    long long sum = 0;
+    long long ans = INF;
+
+    for(long long i=0;i<n;i++){
+        sum += a[i];
+
+        while(sum >= can_tru){
+            ans = min(ans, i - l + 1);
+
+            sum -= a[l];
+            l++;
+        }
+    }
+    if(ans == INF) cout << "-1";
+    else cout << ans;
+    return 0;
+}
