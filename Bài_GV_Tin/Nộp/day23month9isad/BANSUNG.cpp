ad283abbf6350f6a06be8678b89166ef52ecf0fb
93hai <syhai18022012@gmail.com>
2026-09-23 21:47:54 +0700

    ơ

--- Bài_GV_Tin/Nộp/day23month9isad/BANSUNG.cpp
+++ Bài_GV_Tin/Nộp/day23month9isad/BANSUNG.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool check(long long x, long long n, long long a[]){
+    vector<long long> b
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -12,4 +16,9 @@ int main(){
     for(int i=0;i<n;i++){
         cin >> a[i];
     }
+
+    long long x = 1;
+    while(!check(x,n, a)) x++;
+    cout << x;
+    return 0;
 }
