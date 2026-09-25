8a4e47f747331bf19ddce92c837ba21c1adaa841
93hai <syhai18022012@gmail.com>
2026-09-25 14:51:06 +0700

    đang làm b5 và vẫn bug cập nhật pdf nha

--- Bài_GV_Tin/Nộp/day23month9isad/BANSUNG.cpp
+++ Bài_GV_Tin/Nộp/day23month9isad/BANSUNG.cpp
@@ -1,24 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool check(long long x, long long n, long long a[]){
-    vector<long long> b
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n;
-    cin >> n;
+    long long n,k;
+    cin >> n >> k;
 
     vector<long long> a(n);
+
     for(int i=0;i<n;i++){
         cin >> a[i];
     }
 
-    long long x = 1;
-    while(!check(x,n, a)) x++;
-    cout << x;
-    return 0;
+    long long dau = 0;
+    long long cuoi = 2e18;
+
+    while(dau < cuoi){
+        long long giua = (dau + cuoi) / 2;
+
+        
+    }
 }
